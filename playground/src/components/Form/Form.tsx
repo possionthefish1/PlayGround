102a4e1f106373bbae115029d8666410832a9600
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-06-12 15:11:49 +0700

    zod 1 b4 button disabling

--- playground/src/components/Form/Form.tsx
+++ playground/src/components/Form/Form.tsx
@@ -1,16 +1,26 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { type FieldValues, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z
+    .string()
+    .min(6, { message: 'Name must be at least 6 characters' }),
+  // age: z.number().min(18), we don't always need custom msg
+  age: z
+    // biome-ignore lint/style/useNamingConvention: <from Zod>
+    .number({ invalid_type_error: 'Age field is required' })
+    .min(18, { message: 'You must be at least 18 years old' }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   const obSubmit = (data: FieldValues) => console.log(data);
   console.log(errors);
 
@@ -26,32 +36,22 @@ function Form() {
             className='grow rounded border border-slate-500'
             type='text'
             id='username'
-            {...register('name', { required: true, minLength: 6 })}
+            {...register('name')}
           />
-          {errors.name?.type === 'required' && (
-            <p className='text-red-600'>
-              This Username is required.
-            </p>
-          )}
-          {errors.name?.type === 'minLength' && (
-            <p className='text-red-600'>
-              The username must be at least 6 characters.
-            </p>
+          {errors.name && (
+            <p className='text-red-600'>{errors.name.message}</p>
           )}
         </div>
         <div className='mb-3 flex'>
           <label htmlFor='age'>Age</label>
           <input
             className='grow rounded border border-slate-500'
-            {...register('age', { required: true, min: 18 })}
+            {...register('age', { valueAsNumber: true })}
             type='number'
             id='age'
           />
-          {errors.age?.type === 'required' && (
-            <p className='text-red-600'>Age is required.</p>
-          )}
-          {errors.age?.type === 'min' && (
-            <p className='text-red-600'>Wait till you get older.</p>
+          {errors.age && (
+            <p className='text-red-600'>{errors.age.message}</p>
           )}
         </div>
         <button
